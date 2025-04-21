23f50b1d41925736fc0053b1f6f1397716439af0
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-04-21 15:03:05 +0300

    update navbar, projectlist, and projectitem componnets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,22 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+      <a href="#home" key="home">home</a>
+      <a href="#about" key="about">about</a>
+      <a href="#projects" key="projects">projects</a>
+  </nav>
+
+
+  //   <nav>
+  //   {links.map((link) => (
+  //     <a key={link} href={`/${link}`} className="nav-link">
+  //       {link.charAt(0).toUpperCase() + link.slice(1)}
+  //     </a>
+  //   ))}
+  // </nav>
+  );
 }
 
 export default NavBar;
